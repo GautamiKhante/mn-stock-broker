be770f694021f7bb83dca8927d16d1c34f966544
Gautami Khante <GautamiKhante>
2023-07-26 12:22:23 +0000

    Initial commit

---
+++ src/main/java/com/study/home/Application.java
@@ -0,0 +1,10 @@
+package com.study.home;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
